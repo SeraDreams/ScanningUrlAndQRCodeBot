5fad8eeec1703ab4f6f706d04f96f5f0c92eb191
Sera <a-nes.rnd@mail.ru>
2023-05-01 19:42:35 +0300

    Made some minor edits

--- src/convert_qrcode_to_link.py
+++ src/convert_qrcode_to_link.py
@@ -5,8 +5,7 @@ import os
 
 
 def get_link_qr_code(user_id):
-    img = cv2.imread(f'src/img_{user_id}.png')
-    data = pyzbar.decode(img)
+    data = pyzbar.decode(cv2.imread(f'src/img_{user_id}.png'))
     link = data[0].data.decode("utf-8")
     expression = r'(http?://(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?://(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
     regex = re.compile(expression, re.IGNORECASE)
